d8589a811d55d2158be76d9ff4e692ac4a9e585c
Ezea Victor Chukwuebuka <53367180+Victorvikson1996@users.noreply.github.com>
2024-02-15 03:30:57 +0100

    Modification

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,13 @@
-import { View, Text, FlatList, StyleSheet, SafeAreaView } from 'react-native';
+import {
+  FlatList,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+  Image
+} from 'react-native';
 import React from 'react';
-import { Button, imageUrls } from '../components';
+import { imageUrls } from '../components';
 import { Card } from '../components';
-import { Entypo, AntDesign } from '@expo/vector-icons';
 
 export const Home = () => {
   return (
@@ -13,24 +18,14 @@ export const Home = () => {
         keyExtractor={(item, index) => item.toString()}
         numColumns={2}
         columnWrapperStyle={styles.row}
+        showsVerticalScrollIndicator={false}
       />
-      <View
-        style={{
-          borderWidth: 2,
-          borderColor: 'rgba(0,0,0,0.2)',
-          alignItems: 'center',
-          justifyContent: 'center',
-          width: 70,
-          position: 'absolute',
-          bottom: 60,
-          right: 40,
-          height: 70,
-          backgroundColor: '#fff',
-          borderRadius: 100
-        }}
-      >
-        <AntDesign name='plus' size={24} color='black' />
-      </View>
+      <TouchableOpacity activeOpacity={1} style={styles.selector}>
+        <Image
+          source={require('../assets/images/add.png')}
+          style={styles.image}
+        />
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
@@ -40,9 +35,26 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff'
   },
-
   row: {
     flex: 1,
     justifyContent: 'space-around'
+  },
+  selector: {
+    borderWidth: 2,
+    borderColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 70,
+    position: 'absolute',
+    bottom: 60,
+    right: 40,
+    height: 70,
+    backgroundColor: 'black',
+    borderRadius: 100
+  },
+  image: {
+    width: 30,
+    height: 30,
+    tintColor: 'white'
   }
 });
---
+++ src/utils/Colors.tsx
@@ -0,0 +1,6 @@
+export const COLORS = {
+  modalbackgroundcolor: 'rgba(0,0,0,0.5)',
+  modalbackgroundContainerColor: 'white',
+  white: '#FFFFFF',
+  red: '#FF0000'
+};
